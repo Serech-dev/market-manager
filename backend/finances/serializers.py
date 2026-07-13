e7e8c8b99a708f27454f236ee4b6724f42e89105
Serech <briandenett@gmail.com>
2026-07-13 01:34:55 -0300

    finances | redo serializer for better display of category

--- backend/finances/serializers.py
+++ backend/finances/serializers.py
@@ -1,7 +1,17 @@
-from .models import Transaction
 from rest_framework import serializers
 
+from .models import Category, Transaction
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
+
 class TransactionSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
     class Meta:
         model = Transaction
         fields = "__all__"
