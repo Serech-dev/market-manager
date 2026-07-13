3613d710524656b9cdffeaf5c5dc7bcd7c6c035f
Serech <briandenett@gmail.com>
2026-07-13 03:03:45 -0300

    finances | add summary view

--- backend/finances/views.py
+++ backend/finances/views.py
@@ -1,4 +1,8 @@
+from django.db.models import Sum
+
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Transaction
 from .serializers import TransactionSerializer
@@ -8,3 +12,23 @@ class TransactionListCreateView(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
+class TransactionSummaryView(generics.APIView):
+    def get(self, request):
+        queryset = Transaction.objects.all()
+        income = Transaction.objects.filter(
+            category__transaction_type="income"
+            ).aggregate(
+                total=Sum("amount")
+            )["total"]
+        expenses = Transaction.objects.filter(
+                category__transaction_type="expense"
+            ).aggregate(
+                total=Sum("amount")
+            )["total"]
+
+        summary = {
+            "Ingreso" : income or 0
+            "Gasto" : expenses or 0
+            "Balance" : income - expenses
+        }
+        return Response(summary)
