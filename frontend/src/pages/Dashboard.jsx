f455436ae795c75ef1db380627659633323ac777
Serech <briandenett@gmail.com>
2026-07-15 13:52:32 -0300

    implement basic frontend

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+function Dashboard() {
+    return (
+        <>
+            <h1>Market Manager</h1>
+
+            <h2>Today's Summary</h2>
+
+            <p>Gross</p>
+
+            <p>Investment</p>
+
+            <p>Earnings</p>
+        </>
+    );
+}
+
+export default Dashboard;
