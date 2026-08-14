37dc06acb7aefc5419be6663aae307e490be0876
Serech <briandenett@gmail.com>
2026-08-14 14:02:07 -0300

    [front] add default theme to picker

--- frontend/src/themes/themes.js
+++ frontend/src/themes/themes.js
@@ -1,14 +1,18 @@
 export const themes = [
     {
+        id: "default",
+        name: "Light",
+    },
+    {
+        id: "dark",
+        name: "Dark",
+    },
+    {
         id: "pastel",
         name: "Pastel",
     },
     {
         id: "blue",
-        name: "Blue",
-    },
-    {
-        id: "dark",
-        name: "Dark",
+        name: "Azul",
     },
 ];
