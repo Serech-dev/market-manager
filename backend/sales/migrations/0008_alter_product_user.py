2efba34b340139152b5c02819aac73fd18757ef1
Serech <briandenett@gmail.com>
2026-08-17 12:48:58 -0300

    fix migration bug

--- backend/sales/migrations/0008_alter_product_user.py
+++ backend/sales/migrations/0008_alter_product_user.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='product',
             name='user',
-            field=models.ForeignKey(default='0', on_delete=django.db.models.deletion.CASCADE, related_name='products', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='products', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
