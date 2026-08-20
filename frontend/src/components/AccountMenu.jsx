2822dabad086653ffabeaf8e259c9af991697ee9
Serech <briandenett@gmail.com>
2026-08-19 22:50:30 -0300

    [front] change account menu button color

--- frontend/src/components/AccountMenu.jsx
+++ frontend/src/components/AccountMenu.jsx
@@ -61,7 +61,7 @@ function AccountMenu({ user, onLogout }) {
                     rounded-xl
                     border
                     border-[var(--border)]
-                    bg-[var(--background)]
+                    bg-[var(--secondary)]
                     px-3
                     py-2
                     text-sm
